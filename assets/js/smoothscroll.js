2eb346c5f97e2ea709852321184eff3859a438fd
jaseem-v <muhammedjaseem753@gmail.com>
2024-08-17 23:28:29 +0530

    breadcrumb image added

---
+++ assets/js/smoothscroll.js
@@ -0,0 +1,24 @@
+SmoothScroll({
+    // Scrolling Core
+    animationTime: 2000, // [ms]
+    stepSize: 100, // [px]
+
+    // Acceleration
+    accelerationDelta: 50,  // 50
+    accelerationMax: 3,   // 3
+
+    // Keyboard Settings
+    keyboardSupport: true,  // option
+    arrowScroll: 50,    // [px]
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+
+    // Other
+    touchpadSupport: false, // ignore touchpad by default
+    fixedBackground: true,
+    excluded: ''
+})
